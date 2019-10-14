e8009f3126af3dd9fce7aa394f427ac2fed059f5
Tilaxall <meyrembekov.akzhol@yandex.ru>
2019-10-14 21:04:44 +0600

    cl-camp4

--- myfamily.sh
+++ myfamily.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
+export HERO_ID=1
 curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq --arg HERO_ID "$HERO_ID" ' .[] | select (.id == ($HERO_ID|tonumber)) | .connections.relatives' | sed 's/"//g'
-
