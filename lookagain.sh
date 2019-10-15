44c0f8cc12aa5b0ae2222cf4cfa9fcb10458c97a
Tilaxall <meyrembekov.akzhol@yandex.ru>
2019-10-15 17:08:38 +0600

    lookagain.sh update

--- lookagain.sh
+++ lookagain.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #find -name "*.sh" | sed 's/.//' | sed 's/\///'  |sed 's/.sh//g'
-find . -name '*.sh' | cut -f2 -d '.' | sed  's/test//g' | cut -f2 -d 'h' |sed 's#/##g' 
+find . -name '*.sh' | sed 's/^.*\///' | sed 's/.sh//g'
