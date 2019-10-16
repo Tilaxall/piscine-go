e91acef49de84a8294c06f4e6b95ab53c6cdf087
Tilaxall <meyrembekov.akzhol@yandex.ru>
2019-10-16 17:43:54 +0600

    printcomb v2

--- printcomb2.go
+++ printcomb2.go
@@ -4,31 +4,20 @@ import "github.com/01-edu/z01"
 
 func PrintComb2() {
 
-	//var a rune = 48
-	//var b rune = 49
-	//var c rune = 57
-
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
 			for h := '0'; h <= '9'; h++ {
-				for k := '0'; k <= '9'; k++ {
+				for k := h + 1; k <= '9'; k++ {
+
 					z01.PrintRune(i)
 					z01.PrintRune(j)
 					z01.PrintRune(32)
 					z01.PrintRune(h)
 					z01.PrintRune(k)
-					z01.PrintRune(44)
-					z01.PrintRune(32)
-
-					/*if k == '9' {
-						k = '0'
-						if h == '9' {
-							h = '0'
-							if j == '9' {
-								j = '0'
-							}
-						}
-					}*/
+					if !(i == '9' && j == '9' && h == '9' && k == '9') {
+						z01.PrintRune(44)
+						z01.PrintRune(32)
+					}
 
 				}
 			}
