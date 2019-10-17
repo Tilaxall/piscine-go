9ade487abd40b66163bf2aa158541589b81f78c0
Tilaxall <meyrembekov.akzhol@yandex.ru>
2019-10-17 14:09:29 +0600

    Quest-03

---
+++ printstr.go
@@ -0,0 +1,12 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func PrintStr(str string) {
+	strTemp := []rune(str)
+	for i := range strTemp {
+		z01.PrintRune(strTemp[i])
+	}
+}
---
+++ strrev.go
@@ -0,0 +1,16 @@
+package piscine
+
+func PrintStr(str string) string {
+	strTemp := []rune(str)
+	strTempreverse := []rune(str)
+	var number int
+	for i := range strTemp {
+		number = i
+
+	}
+	for i := range strTemp {
+		strTempreverse[i] = strTemp[number]
+		number--
+	}
+	return string(strTempreverse)
+}
