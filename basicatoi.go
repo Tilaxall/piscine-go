e020bacc180f5d37b6d99f0572a933bc0bb1f98d
Tilaxall <meyrembekov.akzhol@yandex.ru>
2019-10-17 21:33:51 +0600

    basicatoi

--- basicatoi.go
+++ basicatoi.go
@@ -4,7 +4,7 @@ func BasicAtoi(s string) int {
 	result := 0
 	for _, value := range s {
 		a := 0
-		for i := '1'; i <= val; i++ {
+		for i := '1'; i <= value; i++ {
 			a++
 		}
 		result = result*10 + a
