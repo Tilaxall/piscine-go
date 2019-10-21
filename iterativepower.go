a8fa059439b757f30f2fddc2381e9501a477ea04
Tilaxall <meyrembekov.akzhol@yandex.ru>
2019-10-21 11:05:56 +0600

    iterativepower update

--- iterativepower.go
+++ iterativepower.go
@@ -2,7 +2,7 @@ package piscine
 
 func IterativePower(nb int, power int) int {
 	var result int = 1
-	if nb == 0 || power <= 0 {
+	if power < 0 {
 		return 0
 	} else {
 		for i := 1; i <= power; i++ {
