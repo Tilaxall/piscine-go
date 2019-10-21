997983cb71625bd5ae181bb04e5893718303cc0e
Tilaxall <meyrembekov.akzhol@yandex.ru>
2019-10-21 18:59:12 +0600

    basicatoi2

--- atoi.go
+++ atoi.go
@@ -1,18 +1,15 @@
 package piscine
 
-func BasicAtoi2(s string) int {
+func Atoi(s string) int {
 	result := 0
 	/*if s[0] == '-' && s[1] == '-' || s[0] == '+' && s[1] == '+' {
 		return 0
 	}*/
-	if s[0] < '0' || s[0] > '9' {
+	if s[1] < '0' || s[0] > '9' {
 		return 0
 	}
 	for _, value := range s {
 		a := 0
-		if value == ' ' {
-			return 0
-		}
 		for i := '1'; i <= value; i++ {
 			a++
 		}
