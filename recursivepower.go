b887a5ee0ca6efdc391f0681828d7aafdbe9e20b
Tilaxall <meyrembekov.akzhol@yandex.ru>
2019-10-21 12:24:50 +0600

    recursivepower update

--- recursivepower.go
+++ recursivepower.go
@@ -2,9 +2,9 @@ package piscine
 
 func RecursivePower(nb int, power int) int {
 	var result int = 1
-	if nb < 0 {
+	if power < 0 {
 		return 0
-	} else if nb >= 1 {
+	} else if power >= 0 {
 		if power > 1 {
 			//nb = nb * nb
 			result = result * nb * RecursivePower(nb, power-1)
