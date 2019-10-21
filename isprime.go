b40cd529dd96f18b2ab8c21882442ebda6934865
Tilaxall <meyrembekov.akzhol@yandex.ru>
2019-10-21 15:16:48 +0600

    isprime update

--- isprime.go
+++ isprime.go
@@ -4,10 +4,14 @@ func IsPrime(nb int) bool {
 	if nb == 1 {
 		return false
 	}
-	for i := 2; i*i <= nb; i++ {
-		if nb%i == 0 {
-			return false
+	if nb <= 0 {
+		return false
+	} else {
+		for i := 2; i*i <= nb; i++ {
+			if nb%i == 0 {
+				return false
+			}
 		}
+		return true
 	}
-	return true
 }
