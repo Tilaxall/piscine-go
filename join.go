6c7fa44ec6deb88b5c7d79a3bb7cc5ae52d0661d
Tilaxall <meyrembekov.akzhol@yandex.ru>
2019-10-23 14:55:44 +0600

    join update

--- join.go
+++ join.go
@@ -3,10 +3,16 @@ package piscine
 func Join(strs []string, sep string) string {
 	var resultStr string
 	strIndex := 0
+	massIndex := 0
+	for index, key := range strs {
+		key = key
+		massIndex = index
+	}
+
 	for _, key := range strs {
 		resultStr = resultStr + key
 		strIndex++
-		if strIndex == len(strs) {
+		if strIndex == massIndex {
 			break
 		}
 		resultStr = resultStr + sep
