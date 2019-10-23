eec459a4ba2bf7c182b93355a767f6477c84fcee
Tilaxall <meyrembekov.akzhol@yandex.ru>
2019-10-23 14:26:12 +0600

    isprintable update

--- isprintable.go
+++ isprintable.go
@@ -2,8 +2,9 @@ package piscine
 
 func IsPrintable(str string) bool {
 	strbool := true
-	for _, key := range str {
-		if !(key >= 'a' && key <= 'z' || key >= 'A' && key <= 'Z' || key >= '0' && key <= '9') {
+	strRune := []rune(str)
+	for _, key := range strRune {
+		if key == '\\' {
 			strbool = false
 		}
 	}
