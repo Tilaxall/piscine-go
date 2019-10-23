7a4c3806aa7ffae718b99e35c89d17c36957207a
Tilaxall <meyrembekov.akzhol@yandex.ru>
2019-10-23 14:03:50 +0600

    isalpha

---
+++ isalpha.go
@@ -0,0 +1,11 @@
+package piscine
+
+func IsAlpha(str string) bool {
+	strbool := true
+	for _, key := range str {
+		if !(key >= 'a' && key <= 'z' || key >= 'A' && key <= 'Z' || key >= '0' && key <= '9') {
+			strbool = false
+		}
+	}
+	return strbool
+}
