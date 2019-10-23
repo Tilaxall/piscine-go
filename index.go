53933ab5f28bcf5d18c47400ea566db8aef0e76d
Tilaxall <meyrembekov.akzhol@yandex.ru>
2019-10-23 09:22:02 +0600

    index

---
+++ index.go
@@ -0,0 +1,53 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	findStr := []rune(toFind)
+	indexStr := []rune(s)
+	index := -1
+	for findIndex, findKey := range findStr {
+		for strIndex, key := range indexStr {
+			if findIndex != 0 && index != -1 && findKey == key {
+				break
+			}
+			if findKey == key && index == -1 {
+				//fmt.Println(findIndex)
+				//fmt.Println(findKey)
+				//fmt.Println(strIndex)
+				//fmt.Println(key)
+				index = strIndex
+				break
+
+			}
+		}
+	}
+
+	/*
+		findIndex := 0
+		findStr := []rune(toFind)
+		for keyIndex, key := range s {
+			if index != 0 {
+				break
+			}
+			if findStr[0] == key {
+				index = keyIndex
+				findIndex = 1
+				findStr = strings.Replace
+			}
+
+		}*/
+
+	/*for i := index; i <= len(s); i++ {
+		for keyIndex, key := range s {
+			if keyIndex >= index {
+				if findStr[findIndex] == key {
+
+				} else {
+					return -1
+				}
+
+			}
+		}
+	}*/
+
+	return index
+}
