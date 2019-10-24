6f2c11e12bccb174919bda23757919eabf0dd22c
Tilaxall <meyrembekov.akzhol@yandex.ru>
2019-10-24 12:36:50 +0600

    splitwhitespaces

---
+++ splitwhitespaces.go
@@ -0,0 +1,26 @@
+package piscine
+
+func SplitWhiteSpaces(str string) []string {
+	Index := 0
+	indexResultMass := 0
+	for index, key := range str {
+		if key == ' ' || !(key >= 0 && key <= 31) {
+			indexResultMass++
+		}
+		Index = index
+	}
+	var tempStr string
+	resultMass := make([]string, indexResultMass-2)
+	for keyIndex, key := range str {
+		if !(key >= 0 && key <= 31) {
+			tempStr += string(key)
+		}
+		if key == ' ' || keyIndex == Index {
+			for i := 0; i <= Index; i++ {
+				resultMass[i] = tempStr
+				break
+			}
+		}
+	}
+	return resultMass
+}
