2fd5f87c076aa256a7f268e22f96bbafceeb76ff
Tilaxall <meyrembekov.akzhol@yandex.ru>
2019-10-24 18:35:59 +0600

    printwordstables

---
+++ printwordstables.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	str := "Hello how are you?"
+	table := SplitWhiteSpaces(str)
+	PrintWordsTables(table)
+}
+
+// SplitWhiteSpaces function separates the words of a string
+// and puts them in a string array.
+// The separators are spaces, tabs and newlines.
+func SplitWhiteSpaces(str string) []string {
+	size := 1
+	var result []string
+	lenstr := 0
+	for i := range str {
+		lenstr = i + 1
+	}
+
+	for i := 0; i < lenstr-1; i++ {
+		if str[i] == ' ' || str[i] == '\t' || str[i] == '\n' {
+			size++
+			if i > 0 {
+				if str[i-1] == ' ' || str[i-1] == '\t' || str[i-1] == '\n' {
+					size--
+				}
+			}
+		}
+	}
+
+	result = make([]string, size)
+
+	tempstr := ""
+	j := 0
+	for i := 0; i <= lenstr; i++ {
+		if i == lenstr {
+			if tempstr != "" {
+				result[j] = tempstr
+			}
+		} else if str[i] != ' ' && str[i] != '\t' && str[i] != '\n' {
+			tempstr = tempstr + string(str[i])
+		} else {
+			if tempstr != "" {
+				result[j] = tempstr
+				j++
+			}
+			tempstr = ""
+		}
+	}
+	return result
+}
+
+func PrintWordsTables(table []string) {
+	for _, key := range table {
+		for _, strKey := range key {
+			z01.PrintRune(strKey)
+		}
+		z01.PrintRune('\n')
+	}
+}
