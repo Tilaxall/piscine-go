1b924581e14bdc6fe2e4c5c054282266beb10363
Tilaxall <meyrembekov.akzhol@yandex.ru>
2019-10-24 11:46:47 +0600

    concatparams

---
+++ concatparams.go
@@ -0,0 +1,13 @@
+package pisine
+
+func ConcatParams(args []string) string {
+	var strMass string
+	for index, key := range args {
+		if index == 0 {
+			strMass = key
+			continue
+		}
+		strMass += "\n" + key
+	}
+	return strMass
+}
